56a50fe82eed5111e855c7b28085d699224d4c5c
HarolVasquez <pvyanque@gmail.com>
2025-05-27 11:17:57 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,7 @@ import './App.css'
 function App() {
   return (
     <>
-      <h1>Mi primera variable de entorno: {import.meta.env.VITE_SOME_KEY}</h1>
-      <h1>Mi segunda variable de entorno: {import.meta.env.VITE_DB_PASSWORD}</h1>
-      <h1>Mi tercera variable de entorno: {import.meta.env.VITE_NEW_KEY2}</h1>
+      <h1>!Hola <span style="color:green">mundo</span>🌎¡</h1>
     </>
   )
 }
