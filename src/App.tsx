7768c725f8fbbd0b27dd247dede86e18076a52ce
HarolVasquez <pvyanque@gmail.com>
2025-05-27 12:03:06 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
   return (
     <>
-      <h1>¡Hola <span style={{ color: 'green', fontStyle: 'italic', fontWeight: 'bold' }}>mundo</span>🌎 con Git Actions!</h1>
+      <h1>¡Hola <span style={{ color: 'green', fontStyle: 'italic', fontWeight: 'bold' }}>mundo</span>🌎 con Jenkins Pipeline!</h1>
     </>
   )
 }
